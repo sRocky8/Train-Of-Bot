8ff211e432d42e84cf5211360de589a7a6a3463e
sRocky8 <sroccapr@student.fitchburgstate.edu>
2019-02-25 01:16:54 -0500

    Added Dialogue Class - NOT IMPLEMENTED

---
+++ Train Of Bot/Assets/Scripts/DialogueClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueClass {
+
+    public string npcName;
+
+    [TextArea(3, 10)]
+    public string[] linesOfDialogue;
+
+}
